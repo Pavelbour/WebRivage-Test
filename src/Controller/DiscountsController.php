8f7393703a87cc17b33f68fc1dd5e3e25859e89a
Pavelbour <pavelbour@gmail.com>
2020-01-15 23:51:12 +0100

    The new discount form was created

--- src/Controller/DiscountsController.php
+++ src/Controller/DiscountsController.php
@@ -1,8 +1,15 @@
 <?php
     namespace App\Controller;
 
+    use App\Entity\DiscountRules;
     use App\Repository\DiscountRulesRepository;
+    use Doctrine\ORM\EntityManagerInterface;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
     class DiscountsController extends AbstractController
@@ -17,5 +24,33 @@
             return $this->render('discounts.html.twig', [
                 'rules' => $rules
             ]);
+        }
+
+        /**
+         * @Route("/discounts/add-new-discount")
+         */
+        public function addNewRule(Request $request)
+        {
+            $rule = new DiscountRules();
+
+            $form = $this->createFormBuilder($rule)
+                ->add('ruleExpression', TextType::class)
+                ->add('discountPercent', IntegerType::class)
+                ->add('save', SubmitType::class, ['label' => 'Ajouter'])
+                ->getForm();
+
+                $form->handleRequest($request);
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $rule = $form->getData();
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($rule);
+                    $em->flush();
+
+                    return $this->redirectToRoute('/discounts');
+                }
+
+                return $this->render('newRuleForm.html.twig', [
+                    'form' => $form->createView()
+                ]);
         }
     }
--- src/Entity/DiscountRules.php
+++ src/Entity/DiscountRules.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\DiscountRulesRepository")
@@ -18,10 +19,16 @@ class DiscountRules
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Type("string")
      */
     private $ruleExpression;
 
     /**
+     * @Assert\GreaterThanOrEqual(1)
+     * @Assert\LessThanOrEqual(50)
+     * @Assert\NotBlank
+     * @Assert\Type("integer")
      * @ORM\Column(type="smallint")
      */
     private $discountPercent;
